0fb86fc6c1c93ace1e3c03ac45e55fe16479c91f
Juan Miguel Ogarrio <jmogarrio@google.com>
2023-06-15 23:48:48 -0400

    Add setup.sh script for xgboost.

---
+++ xgboost/setup.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+
+
+# xgboost dependency that isn't picked up by xgboost
+# This might no longer be necessary now that we're using pipx --install-dependencies,
+# but leaving this here until we can verify.
+# Tracked in: https://github.com/tnc-br/ddf-isoscapes/issues/73
+sudo apt install python3-sklearn
+
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+
+python3 -m venv pyEnv
+source pyEnv/bin/activate
+python -m pip install -r requirements.txt
+
+python -m pip install GDAL==$(gdal-config --version) --global-option=build_ext --global- option="-I/usr/include/gdal"
+
+# Used to install packages directly into virtual environments.
+sudo apt-get install pipx
+
+python -m jupyter serverextension enable --py jupyter_http_over_ws
+python -m jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0
